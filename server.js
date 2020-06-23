51ff742b0c1e999be742580156fbbcc2ebe2a947
Viet <viet@Viets-MBP.attlocal.net>
2020-06-23 11:21:45 -0400

    testing deploying heroku

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static(__dirname + '/dist/pizzeria'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
+    console.log('test');
   res.sendFile(path.join(__dirname + '/dist/pizzeria/index.html'));
 });
 
