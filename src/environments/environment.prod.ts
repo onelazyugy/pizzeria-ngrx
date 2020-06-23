352d38ab79d2100857f5b6061744df6feec79719
Viet <viet@Viets-MBP.attlocal.net>
2020-06-23 10:22:17 -0400

    first commit to push to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  registerUrl: 'https://pizzeria.com/pizzeria/api/v1/user/register',
-  loginUrl: 'https://pizzeria.com/pizzeria/api/v1/user/login',
-  addWingToCartUrl: 'https://pizzeria.com/pizzeria/api/v1/cart/add',
-  removeItemFromCartUrl: 'https://pizzeria.com/pizzeria/api/v1/cart/remove',
-  retrieveCartUrl: 'https://pizzeria.com/pizzeria/api/v1/cart/retrieve',
-  retrieveTotalItemInCartUrl: 'https://pizzeria.com/pizzeria/api/v1/cart/retrieve?isGetCountOnly=true'
+  registerUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/user/register',
+  loginUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/user/login',
+  addWingToCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/add',
+  removeItemFromCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/remove',
+  retrieveCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/retrieve',
+  retrieveTotalItemInCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/retrieve?isGetCountOnly=true'
 };
