91ae5f8dff8aa72155bac03de3621615200ccbf0
Viet <viettangle12@gmail.com>
2020-07-08 00:07:46 -0400

    update api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  registerUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/user/register',
-  loginUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/user/login',
-  addWingToCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/add/wing',
-  removeItemFromCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/remove',
-  retrieveCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/retrieve',
-  retrieveTotalItemInCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/retrieve?isGetCountOnly=true',
-  updateItemFromCartUrl: 'https://pizzera.herokuapp.com/pizzeria/api/v1/cart/update'
+  registerUrl: 'https://pizzeria-backend.herokuapp.com/pizzeria/api/v1/user/register',
+  loginUrl: 'https://pizzeria-backend.herokuapp.com/pizzeria/api/v1/user/login',
+  addWingToCartUrl: 'https://pizzeria-backend.herokuapp.com/pizzeria/api/v1/cart/add/wing',
+  removeItemFromCartUrl: 'https://pizzeria-backend.herokuapp.com/pizzeria/api/v1/cart/remove',
+  retrieveCartUrl: 'https://pizzeria-backend.herokuapp.com/pizzeria/api/v1/cart/retrieve',
+  retrieveTotalItemInCartUrl: 'https://pizzeria-backend.herokuapp.com/pizzeria/api/v1/cart/retrieve?isGetCountOnly=true',
+  updateItemFromCartUrl: 'https://pizzeria-backend.herokuapp.com/pizzeria/api/v1/cart/update'
 };
